1b1a2465a291d1da17984e7185158014f7e0b984
Theetawat <maxclub_thailand@hotmail.com>
2013-01-20 13:42:50 +0700

    firt commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cn320projectEngrLibrary::Application.config.secret_token = 'd04d592148650d64cc76941db0d2c1e4b57f752b45ddbc6a96a6c82420a19c38a765dfff09fe5ea648842aa47cf82c65052f10a73208224bc3c842862994d21b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Cn320projectEngrLibrary::Application.config.session_store :cookie_store, key: '_cn320project-engr-library_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Cn320projectEngrLibrary::Application.config.session_store :active_record_store
