f94dc027ab84a579f48fee5d798672c9362dc2b3
Theetawat <maxclub_thailand@hotmail.com>
2013-03-13 16:05:09 +0700

    returnbook

--- app/models/borrowreturn.rb
+++ app/models/borrowreturn.rb
@@ -1,14 +1,4 @@
 class Borrowreturn < ActiveRecord::Base
-	def Borrowreturn.findBorrow studentid
-		#@books = Array.new
-		#z = studentid.to_i() # studentid is String
-		#counti = 0
-		#while(Borrowreturn.find_by_studentid(z))			
-		#	@books[counti] = Borrowreturn.find_by_studentid(z)
-		#	counti = counti + 1
-    		#end
-		#return @books
-  	end
 	def Borrowreturn.add(bid,stuid)
 		stuidi = stuid.to_i()
 		#find book name via book id
@@ -16,9 +6,42 @@ class Borrowreturn < ActiveRecord::Base
 		bname = book.name
 		bbarcode = book.barcode
 		time = Time.new
-		strtime = time.strftime("%d-%m-%Y")
-		duedate = time + (24*60*60)
-		strduedate = duedate.strftime("%d-%m-%Y")
+		#strtime = time.strftime("%d-%m-%Y")
+		strtime = DateTime.now	
+	
+		# Calculate Change PRICE
+ 		# check type of User
+		#if(stuid > 521)
+		#  typeid = '1' #Bachelor
+		#else
+		#  typeid = '2'
+		#end 
+
+		strduedate = time  - (Changeprice.find_by_id(1).checkoutdate*3600*24)
+		#duedate = time + (24*60*60)
+		#strduedate = duedate.strftime("%d-%m-%Y")
 		Borrowreturn.create!(:studentid=>stuidi,:bookname=>bname,:borrowdate=>strtime,:duedate=>strduedate,:bookid=>bid,:barcode=>bbarcode,:fee=>0)
+	end
+
+	def Borrowreturn.calculatefee(stuid,duedateTime)
+	        student = Borrowreturn.find_by_studentid(stuid)
+
+		# check type of User
+		#if(stuid > 521)
+		#  typeid = '1' #Bachelor
+		#else
+		#  typeid = '2'
+		#end 
+
+		duedateTime = student.duedate
+		presentTime = DateTime.now
+		if(duedateTime >= presentTime)
+		  0
+		else
+		  #showfee = ((presentTime - duedateTime) / 3600*24) * Changeprice.find_by_id(1).price
+		  #x = presentTime
+		  x = ((duedateTime - presentTime) / 3600*24) * Changeprice.find_by_id(1).price
+		end
+		  
 	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130308090730) do
+ActiveRecord::Schema.define(:version => 20130311085007) do
 
   create_table "adminlists", :force => true do |t|
     t.string "username"
@@ -41,6 +41,16 @@ ActiveRecord::Schema.define(:version => 20130308090730) do
     t.integer "checkoutdate"
     t.float   "price"
   end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "userlists", :force => true do |t|
     t.integer "stucolid"
